8f463e1e5dc477941aee902ceb768be5742e5c68
Bjørn Bråthen <bjorbrat88@gmail.com>
2015-09-24 15:46:32 +0200

    Cleanup gac tests.

---
+++ src/algorithms/gac_tests/vertex_coloring/test_gac_vertex_coloring.py
@@ -0,0 +1,40 @@
+import unittest
+from src.algorithms.gac.gac import GAC
+
+
+class TestGACVertexColoring(unittest.TestCase):
+    def setUp(self):
+
+        self.variables = ['v1', 'v2', 'v3', 'v4']
+
+        self.constraints = [
+            'v1 != v2',
+            'v2 != v3',
+            'v3 != v4',
+            'v4 != v1'
+        ]
+
+        self.domains = {
+            'v1': [0],
+            'v2': [0, 1],
+            'v3': [0, 1],
+            'v4': [0, 1]
+        }
+
+        self.solution = {
+            'v1': [0],
+            'v2': [1],
+            'v3': [0],
+            'v4': [1]
+        }
+
+        self.gac = GAC()
+
+        self.gac.initialize(self.variables, self.domains, self.constraints)
+
+    def test_solve(self):
+        results = self.gac.domain_filtering()
+        self.assertEqual(results, self.solution)
+
+if __name__ == '__main__':
+    unittest.main()
