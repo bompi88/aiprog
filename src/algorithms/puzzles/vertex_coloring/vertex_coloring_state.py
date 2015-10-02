f36cb453af51e843f33dd6fef4eb6ab2c54e701c
Bjørn Bråthen <bjorbrat88@gmail.com>
2015-10-02 16:11:38 +0200

    Cleans up the heuristics and how the successors are being generated. A big performance boost :)

--- src/algorithms/puzzles/vertex_coloring/vertex_coloring_state.py
+++ src/algorithms/puzzles/vertex_coloring/vertex_coloring_state.py
@@ -1,7 +1,6 @@
 """ Specialization of SearchState """
 from collections import OrderedDict
 from src.utils.domaincopy import domaincopy
-import itertools
 import math
 
 from src.algorithms.astar.search_state import SearchState
@@ -37,18 +36,7 @@ class VertexColoringState(SearchState):
     def heuristic_evaluation(self):
         sum_h = 0
 
-        # Give penalty for not being a neighbor
-        if self.last_variable:
-            variables_tbc_old = set(itertools.chain(*[x.variables for x in self.gac.variable_map[self.last_variable]]))
-            if self.new_variable not in variables_tbc_old:
-                sum_h += math.log(20)
-
-        # Give credits for being the most constrained vertex
-        if self.new_variable:
-            variables_tbc_new = set(itertools.chain(*[x.variables for x in self.gac.variable_map[self.new_variable]]))
-            sum_h -= math.log(len(variables_tbc_new) * 20)
-
-        # Look at the solotion domain, and use log to transform
+        # Look at the solution domain, and use log to transform
         # it to simple additions in the solution space
         for domain in self.domains.values():
             sum_h += math.log(len(domain))
@@ -78,27 +66,23 @@ class VertexColoringState(SearchState):
     def generate_all_successors(self):
         successors = []
 
-        # TODO Guess the smartest child
-
         viable_domains = {
             key: domain for key, domain in self.domains.items() if len(domain) > 1
         }
 
-        # foo = OrderedDict(sorted(foo.iteritems(), key=lambda x: x[1]['depth']))
+        sorted_domains = OrderedDict(sorted(viable_domains.items(), key=lambda x: -len(x[1])))
 
-        sorted_domains = OrderedDict(sorted(viable_domains.items(), key=lambda x: (-len(self.gac.variable_map[x[0]]), len(x[0]))))
+        (key, domain) = sorted_domains.popitem()
 
-        for key, domain in sorted_domains.items():
-            for color in domain:
-                new_domains = domaincopy(self.domains)
-                new_domains[key] = [color]
+        for color in domain:
+            new_domains = domaincopy(self.domains)
+            new_domains[key] = [color]
 
-                successor = VertexColoringState(self.state, self.gac, self.num_colors, new_domains, self._solution_length + 1, key, self.new_variable)
-                result = self.gac.rerun(new_domains, key)
+            successor = VertexColoringState(self.state, self.gac, self.num_colors, new_domains, self._solution_length + 1, key, self.new_variable)
+            result = self.gac.rerun(new_domains, key)
 
-                if result:
-                    successor.h = successor.heuristic_evaluation()
-                    successors.append(successor)
+            if result:
+                successors.append(successor)
 
         return successors
 
