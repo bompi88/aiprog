1bd493c9a5fbff03d230783464413a75b8807f0c
Bjørn Bråthen <bjorbrat88@gmail.com>
2015-10-04 17:02:35 +0200

    Created report templates for module 2 and 3.

--- doc/project1/report_builder.sh
+++ doc/project1/report_builder.sh
@@ -5,7 +5,15 @@ pdflatex -output-directory=tmp module_1 > /dev/null
 echo "Moving module 1"
 mv tmp/module_1.pdf pdfs/
 
-echo "Making module 2(not yet..)"
+echo "Making module 2"
+pdflatex -output-directory=tmp module_2 > /dev/null
+echo "Moving module 2"
+mv tmp/module_2.pdf pdfs/
+
+echo "Making module 3"
+pdflatex -output-directory=tmp module_3 > /dev/null
+echo "Moving module 3"
+mv tmp/module_3.pdf pdfs/
 
 echo "Making Project 1"
 pdflatex -output-directory=tmp project > /dev/null
