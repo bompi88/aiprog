8dea31e4de89ed3fb2fec393a1935b41e4048300
Kristian Våge <github@krisvage.com>
2015-10-04 21:29:55 +0200

    Starts documenting module3

--- doc/project1/module_3/code_snippets/nonogram.py
+++ doc/project1/module_3/code_snippets/nonogram.py
@@ -1,1 +1,16 @@
-nonogram.py
+class Nonogram(object):
+    def __init__(self, lines):
+        rows, columns = line[0] in lines
+
+        constraints = []
+        variables = set([])
+        
+        for i in range(rows):
+            variables.add 'r' + str(i)
+
+        for i in range(columns):
+            variables.add 'c' + str(i)
+ 
+        for r in range(rows):
+            for c in range(columns):
+                constraints.add NonoConstraint('r{r} [{c}] == c{c} [{r}]')
