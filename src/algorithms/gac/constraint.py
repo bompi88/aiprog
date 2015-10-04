e3e273e04fe1ffb78a6ae509378a43470c51ab71
Bjørn Bråthen <bjorbrat88@gmail.com>
2015-10-04 15:45:15 +0200

    Cleanup in constraint classes.

--- src/algorithms/gac/constraint.py
+++ src/algorithms/gac/constraint.py
@@ -1,24 +1,58 @@
 """ GAC constraint superclass """
+from src.algorithms.gac.func import make_function
+from copy import deepcopy
+
 
 class Constraint(object):
     """ Object holding an expression with variables, and implements
     a revise method tailored to the problem. """
 
-    def __init__(self):
+    def __init__(self, expression):
         self.variables = []
         self.expression = None
         self.function = None
 
+        self.gen_expressions(expression)
+        self.parse_vars()
+        self.create_func()
+
     def create_func(self):
-        """ Creates the runtime method to evaluate the constraint """
-        raise NotImplementedError(
-            'Implement create_func() in Constraint subclass')
+        self.function = {
+            self.variables[0]: make_function(self.variables, self.expression[0]),
+            self.variables[1]: make_function(self.variables, self.expression[1])
+        }
 
     def revise(self, v, c, domains):
-        """ Evaluates the constraint with the given variables """
-        raise NotImplementedError('Implement evaluate() in Constraint subclass')
+        if v == self.variables[0]:
+            return self.reduce(self.variables[0], self.variables[1], domains)
+        elif v == self.variables[1]:
+            return self.reduce(self.variables[1], self.variables[0], domains)
+
+    def reduce(self, v1, v2, domains):
+        revised = False
+        want_new = True
+
+        for d1 in domains[v1]:
+            satisfied = False
+            for d2 in domains[v2]:
+                if self.function[v1](d1, d2):
+                    satisfied = True
+                    break
+            if not satisfied:
+                if want_new:
+                    domains[v1] = deepcopy(domains[v1])
+                    want_new = False
+                domains[v1].remove(d1)
+                revised = True
+
+        return revised
 
     def parse_vars(self):
         """ Creates variables from constraint """
         raise NotImplementedError(
             'Implement parse_vars() in Constraint subclass')
+
+    def gen_expressions(self, expression):
+        """ Generates the expressions to create functions of """
+        raise NotImplementedError(
+            'Implement gen_expressions() in Constraint subclass')
--- src/puzzles/vertex_coloring/vertex_constraint.py
+++ src/puzzles/vertex_coloring/vertex_constraint.py
@@ -1,37 +1,21 @@
 from src.algorithms.gac.constraint import Constraint
-from src.algorithms.gac.func import make_function
 
 
 class VertexConstraint(Constraint):
 
     def __init__(self, expression=None):
-        Constraint.__init__(self)
-
-        self.expression = expression
-        self.parse_vars()
-        self.create_func()
-
-    def create_func(self):
-        self.function = make_function(self.variables, self.expression)
-
-    def revise(self, v, c, domains):
-        to_remove = []
-        for variable in c.variables:
-            if v is variable:
-                continue
-
-            for d1 in domains[v]:
-                removable = True
-                for d2 in domains[variable]:
-                    if c.function(d1, d2):
-                        removable = False
-
-                if removable:
-                    to_remove.append(d1)
-
-        if to_remove:
-            domains[v] = [e for e in domains[v] if not e in to_remove]
-        return len(to_remove) > 0
+        Constraint.__init__(self, expression)
 
     def parse_vars(self):
-        self.variables = list(i for i in self.expression.split() if i != '!=')
+        self.variables = [i for i in self.expression[0].split() if self.var(i)]
+
+    def var(self, i):
+        return i != '!='
+
+    def gen_expressions(self, expression):
+        parts = expression.split()
+
+        self.expression = [
+            expression,
+            parts[2] + ' ' + parts[1] + ' ' + parts[0]
+        ]
