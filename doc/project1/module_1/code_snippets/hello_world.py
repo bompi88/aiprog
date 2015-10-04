3e28f5c953666465aca293ae886552e57711cbd1
Bjørn Bråthen <bjorbrat88@gmail.com>
2015-10-04 17:27:10 +0200

    Adds lambda function snippet.

--- doc/project1/module_1/code_snippets/hello_world.py
+++
@@ -1,8 +0,0 @@
-class HelloWorldPrintable(object):
-  def __init__(self):
-    self.string = 'hello, world!'
-
-  def __str__(self):
-    return self.string
-
-print(HelloWorldPrintable())
--- doc/project1/module_2/code_snippets/make_functions.py
+++ doc/project1/module_2/code_snippets/make_functions.py
@@ -1,6 +1,3 @@
-""" Creates an anonymous lambda function from expression string """
-
-
 def make_function(variable_names, expression, environment=globals()):
     """ Returns lambda given by expression and variables """
     arguments = ','.join([variable for variable in variable_names])
