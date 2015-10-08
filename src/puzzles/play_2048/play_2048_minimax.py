3716e15cd8c06134fa61852071451ce9ef93a871
Bjørn Bråthen <bjorbrat88@gmail.com>
2015-10-08 08:44:43 +0200

    Progressive deepening. Depth 7.

--- src/puzzles/play_2048/play_2048_minimax.py
+++ src/puzzles/play_2048/play_2048_minimax.py
@@ -3,7 +3,7 @@ class Play2048Minimax(object):
         self.actions = {'left': [-1, 0], 'up': [0, -1],
                         'right': [1, 0], 'down': [0, 1]}
 
-        self.depth = 6
+        self.depth = 7
 
     def minimax_decision(self, state):
         max_value = float('-inf')
@@ -13,7 +13,7 @@ class Play2048Minimax(object):
             result = state.perform(a)
             if result is None:
                 continue
-            value = self.max_value(result, 0, float('-inf'), float('inf'))
+            value = self.max_value(result, self.depth, float('-inf'), float('inf'))
 
             if value > max_value:
                 max_value = value
@@ -25,29 +25,32 @@ class Play2048Minimax(object):
         if depth == 0:
             float('-inf')
 
-        if state.is_terminal() or depth == self.depth:
+        if state.is_terminal():
             return state.utility()
 
         v = float('-inf')
 
-        for s in state.successors():
-            v = max(v, self.min_value(s, depth + 1, alpha, beta))
-            alpha = max(alpha, v)
-            if beta <= alpha:
-                break
+        if depth > 0:
+            for s in state.successors():
+                v = max(v, self.min_value(s, depth - 1, alpha, beta))
+                alpha = max(alpha, v)
+                if beta <= alpha:
+                    break
         return v
 
     def min_value(self, state, depth, alpha, beta):
         if depth == 0:
             return float('inf')
 
-        if state.is_terminal() or depth == self.depth:
+        if state.is_terminal():
             return state.utility()
 
         v = float('inf')
-        for s in state.successors():
-            v = min(v, self.max_value(s, depth + 1, alpha, beta))
-            beta = min(beta, v)
-            if beta <= alpha:
-                break
+
+        if depth > 0:
+            for s in state.successors():
+                v = min(v, self.max_value(s, depth - 1, alpha, beta))
+                beta = min(beta, v)
+                if beta <= alpha:
+                    break
         return v
