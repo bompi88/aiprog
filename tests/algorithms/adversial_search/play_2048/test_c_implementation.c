a1cc8f1236e88a8c2daba13df3e9687fc82cbae7
Bjørn Bråthen <bjorbrat88@gmail.com>
2015-10-20 14:53:27 +0200

    Adds two tests.

--- tests/algorithms/adversial_search/play_2048/test_c_implementation.c
+++ tests/algorithms/adversial_search/play_2048/test_c_implementation.c
@@ -1,9 +1,60 @@
 #include <check.h>
 #include <stdlib.h>
 
-START_TEST (test_create_board)
+START_TEST (test_copy_and_edit_new_array)
     {
-        ck_assert_int_eq(1, 1);
+        int array[16] = {1, 2, 3, 4};
+        int solution_array[16] = {1, 2, 3, 4};
+        int solution_array_new[16] = {4, 3, 2, 1};
+
+        int* array_new = (int*) malloc(sizeof(int)*16);
+        memcpy(array_new, array, sizeof(int)*16);
+
+        array_new[0] = 4;
+        array_new[1] = 3;
+        array_new[2] = 2;
+        array_new[3] = 1;
+
+        ck_assert_int_eq(array_new[0], solution_array_new[0]);
+        ck_assert_int_eq(array_new[1], solution_array_new[1]);
+        ck_assert_int_eq(array_new[2], solution_array_new[2]);
+        ck_assert_int_eq(array_new[3], solution_array_new[3]);
+
+        ck_assert_int_eq(array[0], solution_array[0]);
+        ck_assert_int_eq(array[1], solution_array[1]);
+        ck_assert_int_eq(array[2], solution_array[2]);
+        ck_assert_int_eq(array[3], solution_array[3]);
+    }
+END_TEST
+
+START_TEST (test_array_of_arrays)
+    {
+        int array[16] = {1, 2, 3, 4};
+        int solution_array[16] = {1, 2, 3, 4};
+        int solution_array_new[16] = {4, 3, 2, 1};
+
+        int* array_new = (int*) malloc(sizeof(int)*16);
+        memcpy(array_new, array, sizeof(int)*16);
+
+        array_new[0] = 4;
+        array_new[1] = 3;
+        array_new[2] = 2;
+        array_new[3] = 1;
+
+        int** array_of_arrays = (int**) malloc(sizeof(int*)*2);
+
+        array_of_arrays[0] = array;
+        array_of_arrays[1] = array_new;
+
+        ck_assert_int_eq(array_of_arrays[1][0], solution_array_new[0]);
+        ck_assert_int_eq(array_of_arrays[1][1], solution_array_new[1]);
+        ck_assert_int_eq(array_of_arrays[1][2], solution_array_new[2]);
+        ck_assert_int_eq(array_of_arrays[1][3], solution_array_new[3]);
+
+        ck_assert_int_eq(array_of_arrays[0][0], solution_array[0]);
+        ck_assert_int_eq(array_of_arrays[0][1], solution_array[1]);
+        ck_assert_int_eq(array_of_arrays[0][2], solution_array[2]);
+        ck_assert_int_eq(array_of_arrays[0][3], solution_array[3]);
     }
 END_TEST
 
@@ -14,7 +65,8 @@ Suite *expectimax_suite(void) {
     s = suite_create("Expectimax");
     tc_core = tcase_create("Core");
 
-    tcase_add_test(tc_core, test_create_board);
+    tcase_add_test(tc_core, test_copy_and_edit_new_array);
+    tcase_add_test(tc_core, test_array_of_arrays);
     suite_add_tcase(s, tc_core);
 
     return s;
