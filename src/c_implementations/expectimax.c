2ec6170ea5d16d6447bdfcbadcfb3b6cb6665e43
Bjørn Bråthen <bjorbrat88@gmail.com>
2015-10-21 21:40:46 +0200

    Adds corner heuristics.

--- src/c_implementations/expectimax.c
+++ src/c_implementations/expectimax.c
@@ -295,8 +295,46 @@ double evaluation_function(int* board) {
         return 0;
     }
 
-    // TODO: Write heuristics
+    int mask[16*4] = {
+                        // Top left
+                        10, 9, 8, 7,
+                        9, 6, 5, 4,
+                        8, 5, 3, 2,
+                        7, 4, 2, 1,
+                        // Top right
+                        7, 8, 9, 10,
+                        4, 5, 6, 9,
+                        2, 3, 5, 8,
+                        1, 2, 4, 7,
+                        // Bottom right
+                        1, 2, 4, 7,
+                        2, 3, 5, 8,
+                        4, 5, 6, 9,
+                        7, 8, 9, 10,
+                        // Bottom left
+                        7, 4, 2, 1,
+                        8, 5, 3, 2,
+                        9, 6, 5, 4,
+                        10, 9, 8, 7};
 
+    int grid_component = INT_MIN;
+
+    int h = 0;
+    for(int k=0; k<4; k++) {
+        for (int y = 0; y < 4; y++) {
+            for (int x = 0; x < 4; x++) {
+                int tile = 4 * y + x;
+                h += (int)mask[(k * 16) + tile] * (int)board[tile];
+
+                if(h>grid_component) {
+                    grid_component = h;
+                }
+                double free_tiles_component = amount_of_successors(board) / (double) 16;
+
+                return ((double)grid_component) / free_tiles_component;
+            }
+        }
+    }
     return 1;
 }
 
@@ -405,8 +443,10 @@ int decision(int* board, int depth) {
     return max_action;
 }
 
-//int main() {
-//    int arr[16] = {3, 4, 0, 1, 2, 4, 3, 5, 0, 0, 0, 0, 0, 0, 0, 0};
-//    printf("%d\n", INT_MIN);
-//    printf("%d\n", decision(arr, 5));
-//}
+int main() {
+    int arr[16] = {3, 4, 0, 1,
+                   2, 4, 3, 5,
+                   0, 0, 0, 0,
+                   0, 0, 0, 0};
+    printf("%d\n", decision(arr, 5));
+}
