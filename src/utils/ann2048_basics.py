ff93b011b9726fcd2e5cd2ef3f44b2e079133125
Kristian Våge <github@krisvage.com>
2015-11-23 09:51:38 +0100

    Util function for mirroring data

--- src/utils/ann2048_basics.py
+++ src/utils/ann2048_basics.py
@@ -1,5 +1,6 @@
 import res.play2048s.ai_runs
 import pickle
+import numpy as np
 from copy import deepcopy
 
 
@@ -31,6 +32,39 @@ def load_2048_example(min_tile=1):
         labels.append(line[0])
 
     return features, labels
+
+
+def eightway_mirror(states):
+    return_states = []
+
+    for state in states:
+        label = state[0]
+        feature = state[1]
+
+        mirrored = list_mirror(feature)
+
+        for new_feature in mirrored:
+            return_states.append((label, new_feature))
+
+    return return_states
+
+
+def list_mirror(feature):
+    eight_states = []
+
+    np_arr = np.array(feature)
+
+    np_matrix = np_arr.reshape((4, 4))
+    flipped = np.fliplr(np_matrix)
+
+    eight_states.append(list(np_arr))
+    eight_states.append(list(flipped.reshape(16)))
+
+    for rot in range(1, 4):
+        eight_states.append(list(np.rot90(np_matrix, rot).reshape(16)))
+        eight_states.append(list(np.rot90(flipped, rot).reshape(16)))
+
+    return eight_states
 
 
 def process_states(states):
--- tests/puzzles/play_2048/test_pickled_states.py
+++ tests/puzzles/play_2048/test_pickled_states.py
@@ -1,13 +1,15 @@
 import pickle
+from src.utils.ann2048_basics import eightway_mirror
 
 import res.play2048s
 
-filename = '1447941815.p'
+filename = '1447966849.p'
 
 f = open(res.play2048s.__path__[0] + '/' + filename, 'rb')
 
 states = pickle.load(f)
 
-for states in states:
-    print(states[0], end=' => ')
-    print(states[1])
+for i, state in enumerate(eightway_mirror(states)):
+    print(i, end=': ')
+    print(state[0], end=' => ')
+    print(state[1])
